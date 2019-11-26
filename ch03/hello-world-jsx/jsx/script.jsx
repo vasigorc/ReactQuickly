e66fd5a28612f87f5a24c3d3f3ddde1eb8ab7325
Vasile Gorcinschi <vasigorc@gmail.com>
2019-11-25 22:55:11 -0500

    ch03

---
+++ ch03/hello-world-jsx/jsx/script.jsx
@@ -0,0 +1,25 @@
+let helloWorldReactElement = <h1>Hello world!</h1>
+class HelloWorld extends React.Component {
+    render() {
+        return <h1 {...this.properties}>
+            Hello {this.properties.frameworkName} world!!!
+        </h1>
+    }
+}
+ReactDOM.render (
+    <div>
+        <HelloWorld 
+        id='ember'
+        frameworkName='Ember.js' 
+        title='A framework for creating ambitious web applications.' />,
+        <HelloWorld
+            id='backbone'
+            frameworkName='Backbone.js'
+            title='Backbone.js gives structure to web applications.' />,
+        <HelloWorld 
+        id='angular'
+        frameworkName='Angular.js'
+        title='Superheroic JavaScript MVW Framework.' />
+    </div>,
+    document.getElementById('content')
+)
