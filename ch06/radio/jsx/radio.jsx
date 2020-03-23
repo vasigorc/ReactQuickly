85bc019c4cfb38c627aba577f65c02816271b9e2
Vasile Gorcinschi <vasigorc@gmail.com>
2020-03-22 22:52:04 -0400

    ch06: Finalized radio subproject

--- ch06/radio/jsx/radio.jsx
+++ ch06/radio/jsx/radio.jsx
@@ -44,16 +44,19 @@ class Radio extends React.Component {
     // render one radio button <div />
     render() {
         return <div>
-            <div className="radio-tagger" style={this.state.taggerStyle}>
-                <input type="radio" name={this.props.name} id={this.props.id}></input>
-                <label htmlFor={this.props.id}>
-                    <div className="radio-outer" style={this.state.textStyle}>
-                        <div className="radio-inner" style={this.state.innerStyle}>
-                            <div className className="rado-selected" style={this.state.selectedStyle}></div>
-                        </div>
-                    </div>
-                </label>
+      <div className="radio-tagger" style={this.state.taggerStyle}>
+        <input type="radio" name={this.props.name} id={this.props.id}>
+        </input>
+        <label htmlFor={this.props.id}>
+          <div className="radio-text" style={this.state.textStyle}>{this.props.label}</div>
+          <div className="radio-outer" style={this.state.outerStyle}>
+            <div className="radio-inner" style={this.state.innerStyle}>
+              <div className="radio-selected" style={this.state.selectedStyle}>
+              </div>
             </div>
-        </div>
+          </div>
+        </label>
+      </div>
+    </div>
     }
 }
