7ebe4f8bacd1a877ea200bbf7145fdd0cdd8051f
Vasile Gorcinschi <vasigorc@gmail.com>
2020-04-04 16:50:33 -0400

    ch07: Factored-out radio-group into a separate stateless class

---
+++ ch07/elements/js/radio-group.js
@@ -0,0 +1,31 @@
+class RadioGroup extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    capitalize(word) {
+        if (typeof word != 'string') return '';
+        return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+    render() {
+        const namedRadioButtons = this.props.titles.map(name => {
+            return React.createElement(
+                'label',
+                null,
+                React.createElement(Radio, { title: name,
+                    value: this.props.radioGroup[{ name }],
+                    handler: this.props.handler }),
+                this.capitalize(name)
+            );
+        });
+
+        return React.createElement(
+            'div',
+            null,
+            namedRadioButtons
+        );
+    }
+}
+
+module.exports = RadioGroup;
