b891ee7d67e2b0dd852d5b7ff650c1119c5ca559
Vasile Gorcinschi <vasigorc@gmail.com>
2020-04-11 11:06:37 -0400

    ch09: Added reference to static resources

--- ch09/menu/server.js
+++ ch09/menu/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname))
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'))
 })
